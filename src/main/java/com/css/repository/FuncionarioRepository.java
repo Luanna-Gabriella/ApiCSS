e0376c53bc44052709e24aa38a562b1b15174a4d
Luanna Gabriella <seuemail@example.com>
2025-10-23 14:45:33 -0300

    Ajuste no findAll no repository do cliente e do funcionario

--- src/main/java/com/css/repository/FuncionarioRepository.java
+++ src/main/java/com/css/repository/FuncionarioRepository.java
@@ -1,12 +1,11 @@
 package com.css.repository;
 
-import com.css.entity.Cliente;
 import com.css.entity.Funcionario;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface FuncionarioRepository extends JpaRepository<Funcionario, Integer>{
-    @Query("SELECT c FROM Pessoa c WHERE c.fk_id_tipop = 2")
+    @Query("SELECT f FROM Funcionario f WHERE f.fk_id_tipop = 2")
     List<Funcionario> findAllFuncionarios();
 }
